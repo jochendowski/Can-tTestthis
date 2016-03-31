9342c10028037c1b5707c7eb39448d31bf98bcbe
Justin Ochendowski <j.ochendowski@mc-c02kl25lfft1.modcloth.int>
2016-03-31 16:31:40 -0700

    firstTest

---
+++ new1.java
@@ -0,0 +1,14 @@
+
+public class new1 {
+
+	public static void main(String[] args) {
+		returnAndGet person1 = new returnAndGet();
+		person1.setSex("Male");
+		
+		String dick = person1.getSex();
+		
+		System.out.println(dick);
+
+	}
+
+}
